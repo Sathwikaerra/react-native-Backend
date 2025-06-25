c5641a33c0a6756b9ef189a0e826d3046dec3150
Sathwik <b192624@rgukt.ac.in>
2025-06-26 01:26:58 +0530

    books

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -110,7 +110,7 @@ router.delete("/:id",protectRoute,async(req,res)=>{
 router.get("/user",protectRoute,async(req,res)=>{
     try {
         const books=await Book.find({user:req.user._id}).sort({createdAt:-1});
-        res.json(books)
+        res.json({books})
         
     } catch (error) {
          console.log("error in get all books route")
